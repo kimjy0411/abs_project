689b3e70858844bd0f3967dc9b97cbd5dac26fec
kim jae young <kbs00717@gmail.com>
2024-12-17 21:16:23 +0900

    Update model_utils.py

--- abs_utils/model_utils.py
+++ abs_utils/model_utils.py
@@ -4,6 +4,6 @@ def load_yolo_model():
     """
     YOLO 모델 불러오기
     """
-    model_path = "models/best_backup.pt"  # 모델 경로
+    model_path = "models/best_backup.pt2"  # 모델 경로
     model = YOLO(model_path)
     return model
