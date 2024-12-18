c6e5a98cf7cebc7c16f239c82ee454d80cb0adb0
kim jae young <kbs00717@gmail.com>
2024-12-18 16:16:18 +0900

    delete

--- abs_utils/model_utils.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-def load_yolo_model():
-    """
-    YOLO 모델 불러오기
-    """
-    model_path = "models/best_backup.pt2"  # 모델 경로
-    model = YOLO(model_path)
-    return model
